a51fa80e2b0e0e03ce38c175e9f9418a54675c55
Anastasia Pankina <siapankina@gmail.com>
2019-11-23 12:17:18 +0000

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class StaticController < ApplicationController
+  def hello_world
+    #render "hello_world" ##without render it is  implicit rendering 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "hello_world", to: "static#hello_world" #to controller with file name static_controller
+  #and to the method hello_world
 end
